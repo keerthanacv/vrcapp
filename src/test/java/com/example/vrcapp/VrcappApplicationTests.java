8eee882035a8f89f3e3602b43cbe287ea4361e6b
keerthanacv <keerthanaaish456@gmail.com>
2024-05-30 00:54:33 +0530

    Initial commit

---
+++ src/test/java/com/example/vrcapp/VrcappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.vrcapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VrcappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
